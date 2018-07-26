911ee3fef6654583aff533a01277c120bed185eb
Wolfknightx <Wolfknightx@Gmail.com>
2018-07-26 15:30:04 -0500

    win music sounds now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,72 @@
-	
 	//variables which will be used:
 	var PlayerGuess = 0; //this will be the player's guessed amount
 	var gameWins = 0; //total wins
 	var gamelosses = 0; //total losses
 	var targetScore = 0; //this will be the aimed number
+	var audio = new Audio('assets/music/FinalFantasyVII-VictoryFanfare.mp3');
 
 	//jQuery Text will go in here.
 	$().ready(function() {
-		
-		//Before the game function control goes here.
-		randomNumberGen();
-		genCrystalNumbers();
 
-		//In game function here - GAme in play
-		$(".cCryGem1").click(function(){
-			var guessValue = $(this).attr("value");
-			PlayerGuess = parseInt(guessValue) + PlayerGuess;
-			$("#d5LblGuessedTotal").text(PlayerGuess);
-			winLossControl();
-		});
+	    //Before the game function control goes here.
+	    randomNumberGen();
+	    genCrystalNumbers();
+
+	    //In game function here - GAme in play
+	    $(".cCryGem1").click(function() {
+	        var guessValue = $(this).attr("value");
+	        PlayerGuess = parseInt(guessValue) + PlayerGuess;
+	        $("#d5LblGuessedTotal").text(PlayerGuess);
+	        winLossControl();
+	    });
 	});
 
 
 	//Generate random a random number 18-120 & populate the taret score
-	function randomNumberGen(){
-		targetScore = Math.floor(Math.random() * 102) + 18; //set the range
-		//create a random number generator and fill the information in from there.
-			$("#d3LblTargetScore").text(targetScore); //random number pushed in.
-		}
-
-		function genCrystalNumbers(){
-		//the opening jQuery will run through each class item of cCryGem1..
-		$(".cCryGem1").each(function(){
-			var ranNumber = Math.floor(Math.random() * 12) + 1;
-			$(this).attr("value", ranNumber); //adjust image value to the random number.
-		});
+	function randomNumberGen() {
+	    targetScore = Math.floor(Math.random() * 102) + 18; //set the range
+	    //create a random number generator and fill the information in from there.
+	    $("#d3LblTargetScore").text(targetScore); //random number pushed in.
 	}
 
-	function winLossControl(){
-		//This handles checking if the game would be over or not.
-		//if over - then handle the game over
-		if (PlayerGuess > targetScore) {
-			//update the player on how they did and adjust their score.
-			alert("You've exceeded the number! Try again!!");
-			gamelosses += 1;
-			$("#d31LossScore").text(gamelosses);
-			//reload the screen for the new game.
-			randomNumberGen();
-			genCrystalNumbers();
-			//reset the player information to the base
-			PlayerGuess = 0;
-			$("#d5LblGuessedTotal").text(PlayerGuess);
-		}
-		//if spot on then handle game won
-		if (PlayerGuess === targetScore) {
-			//when the player wins runs through the same information.
-			alert("You win!!");
-			gameWins += 1;
-			$("#d31WinsScore").text(gameWins);
-			//reload the screen for the new game.
-			randomNumberGen();
-			genCrystalNumbers();
-			//reset the player information to the base
-			PlayerGuess = 0;
-			$("#d5LblGuessedTotal").text(PlayerGuess);
-		}
+	function genCrystalNumbers() {
+	    //the opening jQuery will run through each class item of cCryGem1..
+	    $(".cCryGem1").each(function() {
+	        var ranNumber = Math.floor(Math.random() * 12) + 1;
+	        $(this).attr("value", ranNumber); //adjust image value to the random number.
+	    });
+	}
+
+	function winLossControl() {
+	    //This handles checking if the game would be over or not.
+	    //if over - then handle the game over
+	    if (PlayerGuess > targetScore) {
+	        //update the player on how they did and adjust their score.
+	        alert("You've exceeded the number! Try again!!");
+	        gamelosses += 1;
+	        $("#d31LossScore").text(gamelosses);
+	        //reload the screen for the new game.
+	        randomNumberGen();
+	        genCrystalNumbers();
+	        //reset the player information to the base
+	        PlayerGuess = 0;
+	        $("#d5LblGuessedTotal").text(PlayerGuess);
+	    }
+	    //if spot on then handle game won
+	    if (PlayerGuess === targetScore) {
+	        //when the player wins runs through the same information.
+	        alert("You win!!");
+	        gameWins += 1;
+	        //play victory music for winning the found.
+	        audio.loop = false;
+	        audio.play();
+	        //set up to only play the song for 
+	        $("#d31WinsScore").text(gameWins);
+	        //reload the screen for the new game.
+	        randomNumberGen();
+	        genCrystalNumbers();
+	        //reset the player information to the base
+	        PlayerGuess = 0;
+	        $("#d5LblGuessedTotal").text(PlayerGuess);
+	    }
 	}
